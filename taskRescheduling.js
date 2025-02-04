d217480dd3e4a1463805f66197435aaedc758ccb
sujoycodes-oss <sujoy.codes8080@protonmail.com>
2025-02-04 16:27:16 +0530

    removed files

--- taskRescheduling.js
+++
@@ -1,6 +0,0 @@
-let toDoList = ["Freshen up", "Meditate", "Exercise", "Breakfast", "Cleaning"];
-toDoList.shift();
-toDoList = ["Wake Up", "Look Up", ...toDoList];
-toDoList[toDoList.length-1] = "Lunch";
-console.log(toDoList)
-
--- todoList.js
+++
@@ -1,4 +0,0 @@
-let toDoList = ["Freshen up", "Meditate", "Exercise", "Breakfast", "Cleaning"];
-toDoList.unshift("Wake up")
-toDoList.pop()
-console.log("Updated to do list - \n"+toDoList)
