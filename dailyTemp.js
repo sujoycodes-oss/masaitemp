f7be43e607898fbdc09030aaefa4a596377df77b
sujoycodes-oss <sujoy.codes8080@protonmail.com>
2025-02-04 14:57:22 +0530

    removed unwanted files

--- dailyTemp.js
+++
@@ -1,8 +0,0 @@
-/// Name - Sujoy Mondol
-
-
-let tempArray = [32,11,22,31,32,17,27]
-console.log("Temperatures over 7 days before manipulation \n" + tempArray);
-let updatedValue = tempArray[3] + 2;
-tempArray[3] = updatedValue;
-console.log("Temperatures over 7 days after manipulation \n"+tempArray)
